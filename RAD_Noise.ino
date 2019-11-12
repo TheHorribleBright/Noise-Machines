65a5ba5d2907e171a88e90481be601a95e2d7946
The Horrible Bright <buster@cerasus.se>
2019-11-12 22:54:12 +0100

    Add files via upload

---
+++ RAD_Noise.ino
@@ -0,0 +1,26 @@
+//*********************************
+//*           RAD Noise           *
+//*  (Random And Digital Noise)   *
+//*                               *
+//*      By Buster Näslund        *
+//*         CC-BY-NC-SA           *
+//*********************************
+
+int analogin1 = A3;
+int analogin2 = A2;
+int pwmout1 = 0;
+int pwmout2 = 1;
+long intensity;
+
+void setup() {
+  pinMode(analogin1, INPUT);
+  pinMode(analogin1, INPUT);
+  pinMode(pwmout1, OUTPUT); 
+  pinMode(pwmout2, OUTPUT); 
+}
+
+void loop() {
+    intensity = map(analogRead(analogin1), 0, 1023, 2, 15);
+      digitalWrite(pwmout1, random(intensity));
+      digitalWrite(pwmout2, random(intensity));
+}
